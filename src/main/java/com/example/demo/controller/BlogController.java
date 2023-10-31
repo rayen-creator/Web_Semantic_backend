003becf7dd04bb84879e8983009615c59a0da160
Acil Farhat <acil.farhat@esprit.tn>
2023-10-31 21:05:15 +0100

    filter by category done

--- src/main/java/com/example/demo/controller/BlogController.java
+++ src/main/java/com/example/demo/controller/BlogController.java
@@ -85,21 +85,76 @@ public class BlogController {
 
         return jsonArray.toString();
     }
-    @GetMapping("/getblogfilter")
-    public String getBlogsfilter() {
+    @GetMapping("/getBlogsbycat/{Category}")
+    public String getBlogsbycat(@PathVariable("Category") String Category) {
 
-//        String qexec = "PREFIX ns: <http://www.semanticweb.org/houssem/ontologies/2023/9/untitled-ontology-3#>\n" +
-//                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-//                "\n" +
-//                "SELECT ?user\n" +
-//                "WHERE {\n" +
-//                "?user rdf:type ns:Admin .\n" +
-//                "}";
+
         String qexec = "PREFIX ns: <http://www.semanticweb.org/houssem/ontologies/2023/9/untitled-ontology-3#>\n" +
                 "SELECT ?blog ?property ?value\n" +
                 "WHERE {\n" +
                 "  ?blog a ns:Blog.\n" +
-                "  ?blog ns:Blog_post_has_author ns:Sarra_Tounsi.\n" + // Add this line to filter by author
+                "  ?blog ns:Category \"" + Category + "\".\n" +  // Use double quotes for the Category
+                "  ?blog ?property ?value.\n" +
+                "}";
+
+        Model model = JenaEngine.readModel("data/freelance.owl");
+
+        QueryExecution qe = QueryExecutionFactory.create(qexec, model);
+        ResultSet results = qe.execSelect();
+
+        // Create a list to hold JSON objects
+        List<JSONObject> jsonObjects = new ArrayList<>();
+
+        // Iterate over the query results
+        while (results.hasNext()) {
+            QuerySolution solution = results.nextSolution();
+
+            // Create a JSON object for each blog
+            JSONObject blogObject = new JSONObject();
+
+            String blog = solution.get("blog").toString();
+            String property = solution.get("property").toString();
+            String value = solution.get("value").toString();
+
+            // Extract property names and values
+            String propertyName = property.substring(property.lastIndexOf("#") + 1);
+            String propertyValue = value;
+
+            // Add property to the blog object
+            blogObject.put(propertyName, propertyValue);
+
+            // Check if the blog object already exists in the list
+            boolean blogExists = false;
+            for (JSONObject jsonObject : jsonObjects) {
+                if (jsonObject.has(blog)) {
+                    jsonObject.getJSONObject(blog).put(propertyName, propertyValue);
+                    blogExists = true;
+                    break;
+                }
+            }
+
+            if (!blogExists) {
+                // Create a new JSON object for the blog and add it to the list
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put(blog, blogObject);
+                jsonObjects.add(jsonObject);
+            }
+        }
+
+        // Convert the list of JSON objects to a JSON array
+        JSONArray jsonArray = new JSONArray(jsonObjects);
+
+        return jsonArray.toString();
+    }
+    @GetMapping("/getblogbyauthor/{authorUri}")
+    public String getblogbyauthor(@PathVariable("authorUri") String authorUri) {
+
+
+        String qexec = "PREFIX ns: <http://www.semanticweb.org/houssem/ontologies/2023/9/untitled-ontology-3#>\n" +
+                "SELECT ?blog ?property ?value\n" +
+                "WHERE {\n" +
+                "  ?blog a ns:Blog.\n" +
+                "  ?blog ns:Blog_post_has_author ns:" + authorUri + ".\n" + // Add this line to filter by author
                 "  ?blog ?property ?value.\n" +
                 "}";
 
@@ -223,8 +278,6 @@ public class BlogController {
 
     @GetMapping("/getcommentspypost/{postUri}")
     public String getCommentsnypost(@PathVariable("postUri") String postUri) {
-        // Define the URI of the post you want to filter comments by
-        // String postUri = "SpecifyThePostURIHere"; // Replace with the actual post URI
 
         String qexec = "PREFIX ns: <http://www.semanticweb.org/houssem/ontologies/2023/9/untitled-ontology-3#>\n" +
                 "SELECT ?comment ?property ?value\n" +
