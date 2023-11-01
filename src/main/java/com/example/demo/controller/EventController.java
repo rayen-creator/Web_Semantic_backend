8ff364514a4bdc7d7707bcc83f9bf243ad93bc65
NourMakdouli <nour.makdouli@esprit.tn>
2023-11-01 15:21:07 +0100

    correcting get reservations

--- src/main/java/com/example/demo/controller/EventController.java
+++ src/main/java/com/example/demo/controller/EventController.java
@@ -20,6 +20,49 @@ import java.util.Set;
 @CrossOrigin(origins = "http://localhost:4200")
 public class EventController {
 
+
+    @GetMapping("/getEventByLabel/{label}")
+    public ResponseEntity<Object> getEventByLabel(@PathVariable String label) {
+        String qexec = "PREFIX ns: <http://www.semanticweb.org/houssem/ontologies/2023/9/untitled-ontology-3#>\n" +
+                "SELECT ?event ?property ?value\n" +
+                "WHERE {\n" +
+                "  ?event a ns:Event.\n" +
+                "  ?event ns:label \"" + label + "\".\n" +  // Use double quotes for the label
+                "  ?event ?property ?value.\n" +
+                "}";
+
+        Model model = JenaEngine.readModel("data/freelance.owl");
+
+        QueryExecution qe = QueryExecutionFactory.create(qexec, model);
+        ResultSet results = qe.execSelect();
+
+        // Create a JSON object to hold the event properties
+        JSONObject eventObject = new JSONObject();
+
+        // Iterate over the query results
+        while (results.hasNext()) {
+            QuerySolution solution = results.nextSolution();
+
+            String event = solution.get("event").toString();
+            String property = solution.get("property").toString();
+            String value = solution.get("value").toString();
+
+            // Extract property names and values
+            String propertyName = property.substring(property.lastIndexOf("#") + 1);
+            String propertyValue = value;
+
+            // Add property to the event object
+            eventObject.put(propertyName, propertyValue);
+        }
+
+        // Check if the event object is empty
+        if (eventObject.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else {
+            // Return the event as a JSON object
+            return ResponseEntity.ok(eventObject.toString());
+        }
+    }
     @GetMapping("/getAllEvents")
     public String getAllEvents() {
         String qexec = "PREFIX ns: <http://www.semanticweb.org/houssem/ontologies/2023/9/untitled-ontology-3#>\n" +
@@ -83,110 +126,7 @@ public class EventController {
 
         return jsonArray.toString();
     }
-
-    @GetMapping("/getEventByLabel/{label}")
-    public ResponseEntity<Object> getEventByLabel(@PathVariable String label) {
-        String qexec = "PREFIX ns: <http://www.semanticweb.org/houssem/ontologies/2023/9/untitled-ontology-3#>\n" +
-                "SELECT ?event ?property ?value\n" +
-                "WHERE {\n" +
-                "  ?event a ns:Event.\n" +
-                "  ?event ns:label \"" + label + "\".\n" +  // Use double quotes for the label
-                "  ?event ?property ?value.\n" +
-                "}";
-
-        Model model = JenaEngine.readModel("data/freelance.owl");
-
-        QueryExecution qe = QueryExecutionFactory.create(qexec, model);
-        ResultSet results = qe.execSelect();
-
-        // Create a JSON object to hold the event properties
-        JSONObject eventObject = new JSONObject();
-
-        // Iterate over the query results
-        while (results.hasNext()) {
-            QuerySolution solution = results.nextSolution();
-
-            String event = solution.get("event").toString();
-            String property = solution.get("property").toString();
-            String value = solution.get("value").toString();
-
-            // Extract property names and values
-            String propertyName = property.substring(property.lastIndexOf("#") + 1);
-            String propertyValue = value;
-
-            // Add property to the event object
-            eventObject.put(propertyName, propertyValue);
-        }
-
-        // Check if the event object is empty
-        if (eventObject.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        } else {
-            // Return the event as a JSON object
-            return ResponseEntity.ok(eventObject.toString());
-        }
-    }
-
-
-    @GetMapping("/getAttendees/{eventLabel}")
-    public String getAttendeesByEventLabel(@PathVariable String eventLabel) {
-        String qexec = "PREFIX ns: <http://www.semanticweb.org/houssem/ontologies/2023/9/untitled-ontology-3#>\n" +
-                "SELECT ?event ?attendee\n" +
-                "WHERE {\n" +
-                "  ?event a ns:Event.\n" +
-                "  ?event ns:label \"" + eventLabel + "\".\n" +  // Use double quotes for the label
-                "  ?event ns:Event_has__attendees ?attendee.\n" +
-                "}";
-
-        Model model = JenaEngine.readModel("data/freelance.owl");
-
-        QueryExecution qe = QueryExecutionFactory.create(qexec, model);
-        ResultSet results = qe.execSelect();
-
-        // Create a list to hold JSON objects
-        List<JSONObject> jsonObjects = new ArrayList<>();
-
-        // Iterate over the query results
-        while (results.hasNext()) {
-            QuerySolution solution = results.nextSolution();
-
-            String attendeeURI = solution.get("attendee").toString();
-
-            // Query for the attendee's complete information
-            String attendeeQuery = "SELECT ?property ?value WHERE { <" + attendeeURI + "> ?property ?value }";
-            QueryExecution attendeeQe = QueryExecutionFactory.create(attendeeQuery, model);
-            ResultSet attendeeResults = attendeeQe.execSelect();
-
-            // Create a new JSON object for each attendee
-            JSONObject attendeeObject = new JSONObject();
-
-            // Add the attendee's URI to the object
-            attendeeObject.put("Attendee", attendeeURI);
-
-            // Iterate over the attendee's properties and values
-            while (attendeeResults.hasNext()) {
-                QuerySolution attendeeSolution = attendeeResults.nextSolution();
-                String property = attendeeSolution.get("property").toString();
-                String value = attendeeSolution.get("value").toString();
-
-                // Extract property names and values
-                String propertyName = property.substring(property.lastIndexOf("#") + 1);
-                String propertyValue = value;
-
-                // Add property to the attendee object
-                attendeeObject.put(propertyName, propertyValue);
-            }
-
-            // Add the attendee object to the list
-            jsonObjects.add(attendeeObject);
-        }
-
-        // Convert the list of JSON objects to a JSON array
-        JSONArray jsonArray = new JSONArray(jsonObjects);
-
-        return jsonArray.toString();
-    }
-
+    @CrossOrigin
     @GetMapping("/getReservations/{eventLabel}")
     public String getReservationsByEventLabel(@PathVariable String eventLabel) {
         String qexec = "PREFIX ns: <http://www.semanticweb.org/houssem/ontologies/2023/9/untitled-ontology-3#>\n" +
@@ -260,16 +200,14 @@ public class EventController {
 
         return jsonArray.toString();
     }
-
+    @CrossOrigin
     @GetMapping("/getAllReservations")
     public String getAllReservations() {
         String qexec = "PREFIX ns: <http://www.semanticweb.org/houssem/ontologies/2023/9/untitled-ontology-3#>\n" +
-                "SELECT DISTINCT ?reservation ?property ?value ?event ?resDate\n" +
+                "SELECT ?reservation ?property ?value\n" +
                 "WHERE {\n" +
-                "  ?event a ns:Event.\n" +
-                "  ?event ns:Event_has_reservation ?reservation.\n" +
+                "  ?reservation a ns:Reservation.\n" +
                 "  ?reservation ?property ?value.\n" +
-                "  ?reservation ns:Res_Date ?resDate.\n" + // Add this line to retrieve Res_Date
                 "}";
 
         Model model = JenaEngine.readModel("data/freelance.owl");
@@ -280,45 +218,44 @@ public class EventController {
         // Create a list to hold JSON objects
         List<JSONObject> jsonObjects = new ArrayList<>();
 
-        // Create a set to store unique reservation URIs
-        Set<String> uniqueReservations = new HashSet<>();
-
         // Iterate over the query results
         while (results.hasNext()) {
             QuerySolution solution = results.nextSolution();
 
-            String eventURI = solution.get("event").toString();
-            String reservationURI = solution.get("reservation").toString();
-            String resDateWithDataType = solution.get("resDate").toString();
+            // Create a JSON object for each event
+            JSONObject reservtionObject = new JSONObject();
 
-            // Remove the datatype information from Res_Date
-            String[] parts = resDateWithDataType.split("\\^\\^");
-            String resDate = parts[0].trim();
+            String reservation = solution.get("reservation").toString();
+            String property = solution.get("property").toString();
+            String value = solution.get("value").toString();
 
-            // Check if the reservation URI is unique
-            if (!uniqueReservations.contains(reservationURI)) {
-                // Add the reservation URI to the set to mark it as seen
-                uniqueReservations.add(reservationURI);
+            // Extract property names and values
+            String propertyName = property.substring(property.lastIndexOf("#") + 1);
+            String propertyValue = value;
 
-                // Create a new JSON object for each reservation
-                JSONObject reservationObject = new JSONObject();
-                reservationObject.put("Reservation", reservationURI);
-                reservationObject.put("Event", eventURI);
-                reservationObject.put("Res_Date", resDate); // Use the cleaned Res_Date
+            // Extract just the date and time part without the extra information
+            if (propertyValue.contains("^^http://www.w3.org/2001/XMLSchema#dateTime")) {
+                propertyValue = propertyValue.split("\\^\\^")[0];
+            }
 
-                // Iterate over the reservation's properties and values
-                String property = solution.get("property").toString();
-                String value = solution.get("value").toString();
+            // Add property to the event object
+            reservtionObject.put(propertyName, propertyValue);
 
-                // Extract property names and values
-                String propertyName = property.substring(property.lastIndexOf("#") + 1);
-                String propertyValue = value;
+            // Check if the event object already exists in the list
+            boolean reservationExists = false;
+            for (JSONObject jsonObject : jsonObjects) {
+                if (jsonObject.has(reservation)) {
+                    jsonObject.getJSONObject(reservation).put(propertyName, propertyValue);
+                    reservationExists = true;
+                    break;
+                }
+            }
 
-                // Add property to the reservation object
-                reservationObject.put(propertyName, propertyValue);
-
-                // Add the reservation object to the list
-                jsonObjects.add(reservationObject);
+            if (!reservationExists) {
+                // Create a new JSON object for the event and add it to the list
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put(reservation, reservtionObject);
+                jsonObjects.add(jsonObject);
             }
         }
 
@@ -327,7 +264,6 @@ public class EventController {
 
         return jsonArray.toString();
     }
-
 }
 
 
