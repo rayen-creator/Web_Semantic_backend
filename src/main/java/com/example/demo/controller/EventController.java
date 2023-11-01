676a87de208f217f35bbfd22191a02dee3dbecb7
NourMakdouli <nour.makdouli@esprit.tn>
2023-11-01 02:49:30 +0100

    eventCtroller

--- src/main/java/com/example/demo/controller/EventController.java
+++ src/main/java/com/example/demo/controller/EventController.java
@@ -1,227 +1,25 @@
 package com.example.demo.controller;
 
-import java.io.ByteArrayOutputStream;
-import java.util.*;
-
-import org.apache.jena.query.*;
+import com.example.demo.tools.JenaEngine;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
 import org.apache.jena.rdf.model.Model;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.example.demo.tools.JenaEngine;
-
-
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 @RestController
-@RequestMapping("/controller")
+@RequestMapping("/eventcontroller")
 @CrossOrigin(origins = "http://localhost:4200")
-public class RestApiController {
+public class EventController {
 
-    @GetMapping("/getblogs")
-    public String getBlogs() {
-
-//        String qexec = "PREFIX ns: <http://www.semanticweb.org/houssem/ontologies/2023/9/untitled-ontology-3#>\n" +
-//                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-//                "\n" +
-//                "SELECT ?user\n" +
-//                "WHERE {\n" +
-//                "?user rdf:type ns:Admin .\n" +
-//                "}";
-        String qexec = "PREFIX ns: <http://www.semanticweb.org/houssem/ontologies/2023/9/untitled-ontology-3#>\n" +
-                "SELECT ?blog ?property ?value\n" +
-                "WHERE {\n" +
-                "  ?blog a ns:Blog.\n" +
-                "  ?blog ?property ?value.\n" +
-                "}";
-
-        Model model = JenaEngine.readModel("data/freelance.owl");
-
-        QueryExecution qe = QueryExecutionFactory.create(qexec, model);
-        ResultSet results = qe.execSelect();
-
-        // Create a list to hold JSON objects
-        List<JSONObject> jsonObjects = new ArrayList<>();
-
-        // Iterate over the query results
-        while (results.hasNext()) {
-            QuerySolution solution = results.nextSolution();
-
-            // Create a JSON object for each blog
-            JSONObject blogObject = new JSONObject();
-
-            String blog = solution.get("blog").toString();
-            String property = solution.get("property").toString();
-            String value = solution.get("value").toString();
-
-            // Extract property names and values
-            String propertyName = property.substring(property.lastIndexOf("#") + 1);
-            String propertyValue = value;
-
-            // Add property to the blog object
-            blogObject.put(propertyName, propertyValue);
-
-            // Check if the blog object already exists in the list
-            boolean blogExists = false;
-            for (JSONObject jsonObject : jsonObjects) {
-                if (jsonObject.has(blog)) {
-                    jsonObject.getJSONObject(blog).put(propertyName, propertyValue);
-                    blogExists = true;
-                    break;
-                }
-            }
-
-            if (!blogExists) {
-                // Create a new JSON object for the blog and add it to the list
-                JSONObject jsonObject = new JSONObject();
-                jsonObject.put(blog, blogObject);
-                jsonObjects.add(jsonObject);
-            }
-        }
-
-        // Convert the list of JSON objects to a JSON array
-        JSONArray jsonArray = new JSONArray(jsonObjects);
-
-        return jsonArray.toString();
-    }
-
-    @GetMapping("/getcompany")
-    public String getcompany() {
-
-//        String qexec = "PREFIX ns: <http://www.semanticweb.org/houssem/ontologies/2023/9/untitled-ontology-3#>\n" +
-//                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-//                "\n" +
-//                "SELECT ?user\n" +
-//                "WHERE {\n" +
-//                "?user rdf:type ns:Admin .\n" +
-//                "}";
-        String qexec = "PREFIX ns: <http://www.semanticweb.org/houssem/ontologies/2023/9/untitled-ontology-3#>\n" +
-                "SELECT ?company ?property ?value\n" +
-                "WHERE {\n" +
-                "  ?company a ns:Company.\n" +
-                "  ?company ?property ?value.\n" +
-                "}";
-
-        Model model = JenaEngine.readModel("data/freelance.owl");
-
-        QueryExecution qe = QueryExecutionFactory.create(qexec, model);
-        ResultSet results = qe.execSelect();
-
-        // Create a list to hold JSON objects
-        List<JSONObject> jsonObjects = new ArrayList<>();
-
-        // Iterate over the query results
-        while (results.hasNext()) {
-            QuerySolution solution = results.nextSolution();
-
-            // Create a JSON object for each blog
-            JSONObject blogObject = new JSONObject();
-
-            String blog = solution.get("company").toString();
-            String property = solution.get("property").toString();
-            String value = solution.get("value").toString();
-
-            // Extract property names and values
-            String propertyName = property.substring(property.lastIndexOf("#") + 1);
-            String propertyValue = value;
-
-            // Add property to the blog object
-            blogObject.put(propertyName, propertyValue);
-
-            // Check if the blog object already exists in the list
-            boolean blogExists = false;
-            for (JSONObject jsonObject : jsonObjects) {
-                if (jsonObject.has(blog)) {
-                    jsonObject.getJSONObject(blog).put(propertyName, propertyValue);
-                    blogExists = true;
-                    break;
-                }
-            }
-
-            if (!blogExists) {
-                // Create a new JSON object for the blog and add it to the list
-                JSONObject jsonObject = new JSONObject();
-                jsonObject.put(blog, blogObject);
-                jsonObjects.add(jsonObject);
-            }
-        }
-
-        // Convert the list of JSON objects to a JSON array
-        JSONArray jsonArray = new JSONArray(jsonObjects);
-
-        return jsonArray.toString();
-    }
-
-    @GetMapping("/getJob")
-    public String getJob() {
-
-//        String qexec = "PREFIX ns: <http://www.semanticweb.org/houssem/ontologies/2023/9/untitled-ontology-3#>\n" +
-//                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-//                "\n" +
-//                "SELECT ?user\n" +
-//                "WHERE {\n" +
-//                "?user rdf:type ns:Admin .\n" +
-//                "}";
-        String qexec = "PREFIX ns: <http://www.semanticweb.org/houssem/ontologies/2023/9/untitled-ontology-3#>\n" +
-                "SELECT ?job ?property ?value\n" +
-                "WHERE {\n" +
-                "  ?job a ns:Job.\n" +
-                "  ?job ?property ?value.\n" +
-                "}";
-
-        Model model = JenaEngine.readModel("data/freelance.owl");
-
-        QueryExecution qe = QueryExecutionFactory.create(qexec, model);
-        ResultSet results = qe.execSelect();
-
-        // Create a list to hold JSON objects
-        List<JSONObject> jsonObjects = new ArrayList<>();
-
-        // Iterate over the query results
-        while (results.hasNext()) {
-            QuerySolution solution = results.nextSolution();
-
-            // Create a JSON object for each blog
-            JSONObject blogObject = new JSONObject();
-
-            String blog = solution.get("job").toString();
-            String property = solution.get("property").toString();
-            String value = solution.get("value").toString();
-
-            // Extract property names and values
-            String propertyName = property.substring(property.lastIndexOf("#") + 1);
-            String propertyValue = value;
-
-            // Add property to the blog object
-            blogObject.put(propertyName, propertyValue);
-
-            // Check if the blog object already exists in the list
-            boolean blogExists = false;
-            for (JSONObject jsonObject : jsonObjects) {
-                if (jsonObject.has(blog)) {
-                    jsonObject.getJSONObject(blog).put(propertyName, propertyValue);
-                    blogExists = true;
-                    break;
-                }
-            }
-
-            if (!blogExists) {
-                // Create a new JSON object for the blog and add it to the list
-                JSONObject jsonObject = new JSONObject();
-                jsonObject.put(blog, blogObject);
-                jsonObjects.add(jsonObject);
-            }
-        }
-
-        // Convert the list of JSON objects to a JSON array
-        JSONArray jsonArray = new JSONArray(jsonObjects);
-
-        return jsonArray.toString();
-    }
     @GetMapping("/getAllEvents")
     public String getAllEvents() {
         String qexec = "PREFIX ns: <http://www.semanticweb.org/houssem/ontologies/2023/9/untitled-ontology-3#>\n" +
@@ -530,28 +328,27 @@ public class RestApiController {
         return jsonArray.toString();
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
